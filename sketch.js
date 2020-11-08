6740a82c532a21aae83bfd84c1546240e6204fc0
yashpatil1480 <65724005+yashpatil1480@users.noreply.github.com>
2020-11-08 16:37:22 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,28 @@
+//Create variables here
+var dog 
+var happydog
+
+
+function preload()
+
+{
+  //load images here
+  dog.addImage('dog.png')
+  happydog.addimage('')
+}
+
+function setup() {
+	createCanvas(800, 700);
+  
+}
+
+
+function draw() {  
+
+  drawSprites();
+  //add styles here
+
+}
+
+
+
